b9b322c0473e5e7f1dc9c764446cde574feffe3d
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-07 00:41:28 +0530

    feat: implement horizontal scroll for education and skills sections

---
+++ pf/src/app/about/info.tsx
@@ -0,0 +1,15 @@
+// vidun-config.ts
+const Engineer = { 
+  Name: "Vidun Shanuka", Location: "Sri Lanka", Age: 21, Experience: "2+ years",
+  Skills: ["Frontend", "Backend", "Cloud", "CI/CD Development", "Machine Learning and AI"],
+  Focus: "Building Scalable Systems and Integrating AI into Software Development",
+  Principle: () => "Ship clean Systems. Automate the Rest..." 
+};
+export default Engineer;
+
+
+
+// Ship clean Systems. Automate the Rest.
+
+
+
